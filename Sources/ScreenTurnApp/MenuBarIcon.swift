4ad7984a4a1f4cfd17929b55e391470a241e1bfe
salt <salt@Mac-mini-3.local>
2026-06-21 16:11:34 +0800

    Align menu bar display icon baseline

--- Sources/ScreenTurnApp/MenuBarIcon.swift
+++ Sources/ScreenTurnApp/MenuBarIcon.swift
@@ -9,7 +9,7 @@ enum MenuBarIcon {
         NSColor.black.setStroke()
 
         let landscape = NSBezierPath(
-            roundedRect: NSRect(x: 1.1, y: 5.9, width: 10.2, height: 6.7),
+            roundedRect: NSRect(x: 1.1, y: 5.4, width: 10.2, height: 6.5),
             xRadius: 1,
             yRadius: 1
         )
@@ -19,14 +19,14 @@ enum MenuBarIcon {
         let landscapeStand = NSBezierPath()
         landscapeStand.lineCapStyle = .round
         landscapeStand.lineWidth = strokeWidth
-        landscapeStand.move(to: NSPoint(x: 6.2, y: 5.8))
-        landscapeStand.line(to: NSPoint(x: 6.2, y: 4.2))
-        landscapeStand.move(to: NSPoint(x: 4.6, y: 4.2))
-        landscapeStand.line(to: NSPoint(x: 7.8, y: 4.2))
+        landscapeStand.move(to: NSPoint(x: 6.2, y: 5.3))
+        landscapeStand.line(to: NSPoint(x: 6.2, y: 3.2))
+        landscapeStand.move(to: NSPoint(x: 4.6, y: 3.2))
+        landscapeStand.line(to: NSPoint(x: 7.8, y: 3.2))
         landscapeStand.stroke()
 
         let portrait = NSBezierPath(
-            roundedRect: NSRect(x: 10.4, y: 2.2, width: 5.5, height: 11.2),
+            roundedRect: NSRect(x: 10.4, y: 5.4, width: 5.5, height: 10.3),
             xRadius: 1,
             yRadius: 1
         )
@@ -36,10 +36,10 @@ enum MenuBarIcon {
         let portraitStand = NSBezierPath()
         portraitStand.lineCapStyle = .round
         portraitStand.lineWidth = strokeWidth
-        portraitStand.move(to: NSPoint(x: 13.15, y: 2.1))
-        portraitStand.line(to: NSPoint(x: 13.15, y: 1.1))
-        portraitStand.move(to: NSPoint(x: 11.75, y: 1.1))
-        portraitStand.line(to: NSPoint(x: 14.55, y: 1.1))
+        portraitStand.move(to: NSPoint(x: 13.15, y: 5.3))
+        portraitStand.line(to: NSPoint(x: 13.15, y: 3.2))
+        portraitStand.move(to: NSPoint(x: 11.75, y: 3.2))
+        portraitStand.line(to: NSPoint(x: 14.55, y: 3.2))
         portraitStand.stroke()
 
         image.unlockFocus()
